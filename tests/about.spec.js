a4b323f14049557acbce2458abaa9bcb642170a3
aPersonWhoExisted <e.klein5@nait.ca>
2023-12-14 16:51:28 +0000

    14 Dec 2023, 09:51

--- tests/about.spec.js
+++ tests/about.spec.js
@@ -1,8 +1,12 @@
-import {describe, it, expect} from 'vitest';
+import { describe, it, expect } from 'vitest';
 
 describe('About this test suite', () => {
     it('should work', () => {
         expect(true).toBe(true);
     })
-})
 
+    it('should calculate 1 + 1', () => {
+        const actual = add(1,2);
+        const expected = 3;
+    })
+})
